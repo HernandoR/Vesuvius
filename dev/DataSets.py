bdc8ec3f702d559c54cba51fca7c389a1881d65e
HernandoR <45709656+HernandoR@users.noreply.github.com>
2023-04-19 18:57:21 +0800

    bad behavier, F0.5~0.228

--- dev/DataSets.py
+++ dev/DataSets.py
@@ -4,10 +4,121 @@ from pathlib import Path
 
 import cv2
 import numpy as np
-import torch
 from cachetools import FIFOCache, cached, LRUCache
 from torch.utils.data import Dataset
-from tqdm import tqdm
+from tqdm.auto import tqdm
+
+
+class CustomDataset(Dataset):
+    """"
+    Custom Dataset for loading images, masks, and labels
+    params:
+        image_sets: list of image paths
+        masks: list of mask paths
+        labels: list of label paths
+        transform: albumentations transform
+        mode: train, valid, or test
+    """
+
+    def __init__(self, image_sets, cfg, masks=None, labels=None, transform=None, mode="train"):
+        self.image_sets = image_sets
+        self.cfg = cfg
+        self.masks = masks
+        self.labels = labels
+        self.transform = transform
+        self.type = mode
+        self.imgLoader = None
+        self.patch_num = []
+        self.preprocess()
+
+    def preprocess(self):
+        if self.imgLoader is None:
+            self.imgLoader = ImgLoader(
+                cache_dir=self.cfg['cache_dir'],
+                data_dir=self.cfg['data_dir'])
+
+        for mask in self.masks:
+            # mask may be path like or numpy array
+            if isinstance(mask, (str, Path)):
+                mask = self.imgLoader.load_from_path(mask)
+
+            x1_num = math.ceil((mask.shape[1] - self.cfg['tile_size']) / self.cfg['stride']) + 1
+            y1_num = math.ceil((mask.shape[0] - self.cfg['tile_size']) / self.cfg['stride']) + 1
+
+            self.patch_num.append((x1_num, y1_num))
+
+        self.patch_num = np.array(self.patch_num)
+
+    def get_gt(self, img_idx):
+        return self.imgLoader.load_from_path(self.labels[img_idx])
+
+    def get_mask(self, img_idx):
+        return self.imgLoader.load_from_path(self.masks[img_idx])
+
+    def __len__(self):
+        x, y = np.transpose(self.patch_num)
+        return x @ y
+
+    def __getitem__(self, idx):
+        # x1, y1, x2, y2 = self.xyxys[idx]
+        img_id = 0
+        patch_id = idx
+        for i, patch_num in enumerate(self.patch_num):
+            if patch_id < patch_num[0] * patch_num[1]:
+                img_id = i
+                break
+            else:
+                patch_id -= patch_num[0] * patch_num[1]
+
+        x1_num, y1_num = self.patch_num[img_id]
+        x1 = (patch_id % x1_num) * self.cfg['stride']
+        y1 = (patch_id // x1_num) * self.cfg['stride']
+        x2 = x1 + self.cfg['tile_size']
+        y2 = y1 + self.cfg['tile_size']
+
+        img = self.imgLoader.load_from_path(self.image_sets[img_id], channel=self.cfg['in_channels'])
+        mask = self.imgLoader.load_from_path(self.masks[img_id])
+        label = self.imgLoader.load_from_path(self.labels[img_id])
+
+        img = img[y1:y2, x1:x2]
+        mask = mask[y1:y2, x1:x2]
+
+        if img.shape[0] != self.cfg['tile_size'] or img.shape[1] != self.cfg['tile_size']:
+            pad_h = self.cfg['tile_size'] - img.shape[0]
+            pad_w = self.cfg['tile_size'] - img.shape[1]
+            img = np.pad(img, ((0, pad_h), (0, pad_w), (0, 0)), mode="constant", constant_values=0)
+            mask = np.pad(mask, ((0, pad_h), (0, pad_w)), mode="constant", constant_values=0)
+
+        if self.type in ["train", "valid"]:
+            label = label[y1:y2, x1:x2]
+            if label.shape[0] != self.cfg['tile_size'] or label.shape[1] != self.cfg['tile_size']:
+                pad_h = self.cfg['tile_size'] - label.shape[0]
+                pad_w = self.cfg['tile_size'] - label.shape[1]
+                label = np.pad(label, ((0, pad_h), (0, pad_w)), mode="constant", constant_values=0)
+
+            data = self.transform(image=img, mask=mask, label=label)
+            label = data["label"].astype(np.float32)
+        else:
+            label = None
+            data = self.transform(image=img, mask=mask)
+
+        # if mask != data["mask"]:
+        #     print("mask is not equal")`
+        #     fig, ax = plt.subplots(1, 2)
+        #     ax[0].imshow(mask)
+        #     ax[1].imshow(data["mask"])
+        #     ax[0].set_title("mask")
+        #     ax[1].set_title("data[mask]")
+        #     plt.show()
+        image = data["image"]
+        mask = data["mask"]
+
+        return image, mask, label, (x1, y1, x2, y2)
+
+        # image = self.image_sets[idx]
+        # data = self.transform(image=image)
+        # image = data['image']
+        # return image
 
 
 class CVDataSet(Dataset):
@@ -138,11 +249,11 @@ class ImageDataset(Dataset):
 
     def preprocess(self):
         # TODO:
-        # there's a problem on current encoding of positions that
-        # it's hard to identify the global idx, therefore,
-        # the getitem is hard to implement
-        for img_idx, img in enumerate(self.img_list):
-            positions = self.find_non_masked_positions(img)
+        # This should pass the mask rather than the image
+
+        for img_idx, mask_path in enumerate(self.mask_list):
+            mask = self.imgLoader.load_from_path(mask_path)
+            positions = self.find_non_masked_positions(mask, chop_size=self.crop_size)
             self.positions.append(positions)
 
     @staticmethod
@@ -160,8 +271,8 @@ class ImageDataset(Dataset):
                 if np.sum(mask[n:n + chop_size, m:m + chop_size]) > 0:
                     positions.append((n, m))
 
-        # Adjust the positions to account for the black edges
-        positions = [(n - pad_height // 2, m - pad_width // 2) for n, m in positions]
+        # # Adjust the positions to account for the black edges
+        # positions = [(n - pad_height // 2, m - pad_width // 2) for n, m in positions]
 
         return positions
 
@@ -193,16 +304,16 @@ class ImageDataset(Dataset):
         mask = self.imgLoader.load_from_path(mask_path)
 
         # Get image dimensions
-        img_width, img_height = img.size
+        img_width, img_height = img.shape[1], img.shape[2]
 
-        assert pos_x + 256 <= img_width, f"bad pos_x {pos_x} {img_width} on img {img_path},index {index}"
-        assert pos_y + 256 <= img_height, f"bad pos_y {pos_y} {img_height} on img {img_path},index {index}"
+        # assert pos_x + 256 <= img_width, f"bad pos_x {pos_x} {img_width} on img {img_path},index {index}"
+        # assert pos_y + 256 <= img_height, f"bad pos_y {pos_y} {img_height} on img {img_path},index {index}"
 
         img_patch = img[:, pos_x:pos_x + 256, pos_y:pos_y + 256]
         mask_patch = mask[pos_x:pos_x + 256, pos_y:pos_y + 256]
 
         # TODO: determine if we need to do this
-        img_patch = img_patch * mask_patch
+        # img_patch = img_patch * mask_patch
 
         if self.data_type in ["train", "valid"]:
             assert self.label_list is not None, f'label list is None'
@@ -211,13 +322,17 @@ class ImageDataset(Dataset):
             label = self.imgLoader.load_from_path(label_path)
             label_patch = label[pos_x:pos_x + 256, pos_y:pos_y + 256]
 
+            img_patch = np.moveaxis(img_patch, 0, 2)
             augmented = self.transforms(image=img_patch, mask=label_patch)
             img_patch = augmented["image"]
+            img_patch = np.moveaxis(img_patch, 2, 0)
             label_patch = augmented["mask"]
 
         else:
+            img_patch = np.moveaxis(img_patch, 0, 2)
             augmented = self.transforms(image=img_patch)
             img_patch = augmented["image"]
+            img_patch = np.moveaxis(img_patch, 2, 0)
             label_patch = -1
 
         return img_patch, label_patch / 255
@@ -226,19 +341,30 @@ class ImageDataset(Dataset):
 class ImgLoader:
 
     def __init__(self, cache_dir: Path = None, data_dir: Path = None):
-        self.cache_dir = None
-        self.data_dir = None
+        self.cache_dir = cache_dir
+        self.data_dir = data_dir
 
-    def load_from_path(self, file_path: str = None):
-        return ImgLoader.load_from_path_static(self.cache_dir, self.data_dir, file_path)
+    def load_from_path(self, file_path: str = None, channel=6, tile_size=224):
+        ori_img = ImgLoader.load_from_path_static(self.cache_dir, self.data_dir, file_path, channel=channel)
+        # pad_h = (tile_size - ori_img.shape[1] % tile_size) % tile_size
+        # img = np.pad(ori_img, ((0, 0), (0, pad_h), (0, pad_w)), mode='constant')
+        # pad_w = (tile_size - ori_img.shape[2] % tile_size) % tile_size
 
+        return ori_img
     @staticmethod
     @cached(cache=FIFOCache(maxsize=10))
-    def load_from_path_static(cache_dir: Path = None, data_dir: Path = None, file_path: str = None):
+    def load_from_path_static(cache_dir: Path = None, data_dir: Path = None, file_path: str = None, channel=6):
+        if isinstance(file_path, Path):
+            file_path = str(file_path)
+
+        assert not file_path.endswith('npy'), f"file path {file_path} is a npy file"
+        assert os.path.exists(data_dir / file_path), f"file path {file_path} does not exist"
+
         path__npy_ = cache_dir / f"{file_path}.npy"
 
         if os.path.exists(path__npy_):
-            img_l = np.load(str(path__npy_))
+            img_l = np.load(str(path__npy_), allow_pickle=True)
+            assert img_l is not None, f"Cached file {path__npy_} is None"
             return img_l
 
         if not os.path.exists(path__npy_.parent):
@@ -246,15 +372,29 @@ class ImgLoader:
 
         if os.path.isfile(data_dir / file_path):
             img_l = cv2.imread(str(data_dir / file_path), 0)
+            assert img_l is not None, f"Image file {data_dir / file_path} is None"
             np.save(str(path__npy_), img_l)
             return img_l
 
         if os.path.isdir(data_dir / file_path):
-            img_l = []
-            files, _ = os.listdir(data_dir / file_path)
-            for file in tqdm(files):
-                img_l.append(ImgLoader.load_from_path_static(cache_dir, data_dir / f"{file_path}/{file}", data_dir))
+            path__npy_ = cache_dir / f"{file_path}_{channel}.npy"
+            if os.path.exists(path__npy_):
+                img_l = np.load(str(path__npy_), allow_pickle=True)
+                assert img_l is not None, f"Cached file {path__npy_} is None"
+                return img_l
 
-            img_l = np.stack(img_l)
+            img_l = []
+            files = os.listdir(data_dir / file_path)
+            mid = len(files) // 2
+            start = mid - channel // 2
+            end = mid + channel // 2
+            assert start >= 0, f"start {start} is less than 0"
+            assert end <= len(files), f"end {end} is greater than {len(files)}"
+
+            files = files[start:end]
+            for file in tqdm(files):
+                img_l.append(ImgLoader.load_from_path_static(cache_dir, data_dir, f"{file_path}/{file}"))
+
+            img_l = np.stack(img_l, axis=2)
             np.save(str(path__npy_), img_l)
             return img_l
